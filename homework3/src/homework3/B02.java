e82f4b080009ed1d3648b26e6cc61876c77b31f2
yury noh <dliam123@naver.com>
2016-10-04 18:05:45 +0900

    FINISH "문제 2번: 	아파트 분양 면적을 제곱미터에서 평으로 변환하는 프로그램"

--- homework3/src/homework3/B02.java
+++ homework3/src/homework3/B02.java
@@ -1,10 +1,39 @@
 package homework3;
+import java.util.*;
 
 public class B02 {
+	double stdMetric_area;
+	double korMetric_area;
+	
+	public void init() {
+		this.input();
+		this.print();
+	}
+	public void input() {
+		Scanner s = new Scanner(System.in);
+		
+		System.out.print("아파트의 분양 면적(제곱미터)을  입력하시오: ");
+		this.stdMetric_area = s.nextDouble();
+		
+		this.korMetric_area = stdMetric_area/3.305;
+	}
+	public void print() {
+		String size = "";
+		
+		if(this.korMetric_area < 15)
+			size = "소형";
+		else if(this.korMetric_area < 30)
+			size = "중소형";
+		else if(this.korMetric_area < 50)
+			size = "중형";
+		else
+			size = "대형";
+		
+		System.out.printf("%.1f평 %s 아파트 입니다.", this.korMetric_area, size);	
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		new B02().init();
 	}
 
 }
