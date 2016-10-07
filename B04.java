bb1cb4629059c8e8388e065f603ac8924b6aeb02
kim <kchs942@naver.com>
2016-10-07 10:46:49 +0900

    update again

--- B04.java
+++ B04.java
@@ -1,7 +1,37 @@
 import java.util.*;
+rt java.util.*;
 
 class B04 {
-  int salary;
+  double salary;
+    double tax;
+      
+        void init() {
+	    this.input();
+	        this.print();
+		  }
+		    void input() {
+		        Scanner s = new Scanner(System.in);
+			    System.out.print("연봉을 입력하시오: ");
+			        this.salary = s.nextDouble();
+				  }
+				    void print() {
+				        if(salary >= 80000000)
+					      this.tax = (int)(this.salary * (37.0f/100.0f));
+					          else if(this.salary >= 40000000)
+						        this.tax = (int)(this.salary * (28.0f/100.0f));
+							    else if(salary >= 10000000)
+							          this.tax = (int)(this.salary * (19.0f/100.0f));
+								      else
+								            this.tax = (int)(this.salary * (9.5f/100.0f));
+									        System.out.println("연봉 금액에 대한 소득세는 " + tax + "만원 입니다.");
+										  }
+										    public static void main(String args[]) {
+										        new B04().init();
+											  }
+											  }
+
+class B04 {
+  double salary;
   double tax;
   
   void init() {
@@ -10,19 +40,18 @@ class B04 {
   }
   void input() {
     Scanner s = new Scanner(System.in);
-    System.out.print("연봉을 입력하시오(만 단위): ");
-    this.salary = s.nextInt();
-    this.print();
+    System.out.print("연봉을 입력하시오: ");
+    this.salary = s.nextDouble();
   }
   void print() {
-    if(salary < 1000)
-      tax = salary * 9.5%;
-    else if(salary >= 1000)
-      tax = salary * 19%;
-    else if(salary >= 4000)
-      tax = salary * 28%;
+    if(salary >= 80000000)
+      this.tax = (int)(this.salary * (37.0f/100.0f));
+    else if(this.salary >= 40000000)
+      this.tax = (int)(this.salary * (28.0f/100.0f));
+    else if(salary >= 10000000)
+      this.tax = (int)(this.salary * (19.0f/100.0f));
     else
-      tax = salary * 37%;
+      this.tax = (int)(this.salary * (9.5f/100.0f));
     System.out.println("연봉 금액에 대한 소득세는 " + tax + "만원 입니다.");
   }
   public static void main(String args[]) {
